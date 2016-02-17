c8c4eb88f382d253d466898d7aa7515be66fc197
Zygmunt Krynicki <me@zygoon.pl>
2016-02-17 19:46:58 +0100

    Add primitive setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setuptools
+
+setup(
+    name="shnibble",
+    version="0.0",
+    packages=["schnibble"],
+    author="Zygmunt Krynicki",
+    author_email="me@zygoon.pl",
+    description="Toolkit for processing Python bytecode",
+)
