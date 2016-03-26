32a473dae80082219f4991db3f885a363ab21fd7
Zygmunt Krynicki <me@zygoon.pl>
2016-03-26 21:35:00 +0100

    Add ELF section constants
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- schnibble/elf.py
+++ schnibble/elf.py
@@ -59,6 +59,36 @@ PT_X = 1 << 0
 PT_W = 1 << 1
 PT_R = 1 << 2
 
+SHN_UNDEF = 0
+SHN_LORESERVE = 0xff00
+SHN_LOPROC = 0xff00
+SHN_HIPROC = 0xff1f
+SHN_ABS = 0xfff1
+SHN_COMMON = 0xfff2
+SHN_HIRESERVE = 0xffff
+
+SHT_NULL = 0
+SHT_PROGBITS = 1
+SHT_SYMTAB = 2
+SHT_STRTAB = 3
+SHT_RELA = 4
+SHT_HASH = 5
+SHT_DYNAMIC = 6
+SHT_NOTE = 7
+SHT_NOBITS = 8
+SHT_REL = 9
+SHT_SHLIB = 10
+SHT_DYNSYM = 11
+SHT_LOPROC = 0x70000000
+SHT_HIPROC = 0x7fffffff
+SHT_LOUSER = 0x80000000
+SHT_HIUSER = 0xffffffff
+
+SHF_WRITE = 0x1
+SHF_ALLOC = 0x2
+SHF_EXECINSTR = 0x4
+SHF_MASKPROC = 0xf0000000
+
 
 class Header32(ctypes.Structure):
     """ELF header for 32 bit architectures."""
