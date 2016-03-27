cdde6f003532caf43f0f6cc3e9bb82154651b657
Zygmunt Krynicki <me@zygoon.pl>
2016-03-28 01:21:55 +0200

    Initial work on elf builder
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- schnibble/elf.py
+++ schnibble/elf.py
@@ -3,9 +3,10 @@
 
 from __future__ import absolute_import, print_function
 
+import copy
 import ctypes
 
-
+# Indexes into the Elf32_Ehdr.e_ident array
 EI_MAG0 = 0x00
 EI_MAG1 = 0x01
 EI_MAG2 = 0x02
@@ -17,23 +18,32 @@ EI_OSABI = 0x07
 EI_ABIVERSION = 0x08
 EI_PAD = 0x09
 
+# Constants for Elf32_Ehdr.e_ident at indices EI_MAG0, EI_MAG1, EI_MAG2 and
+# EI_MAG3 (sequentially)
+ELFMAG0 = 0x7f
+ELFMAG1 = ord('E')
+ELFMAG2 = ord('L')
+ELFMAG3 = ord('F')
+
+# Constants for Elf32_Ehdr.e_ident at index EI_CLASS
 ELFCLASSNONE = 0
 ELFCLASS32 = 1
 ELFCLASS64 = 2
 
+# Constants for Elf32_Ehdr.e_ident at index EI_DATA
 ELFDATANONE = 0
 ELFDATA2LSB = 1
 ELFDATA2MSB = 2
 
+# Constants for Elf32_Ehdr.e_ident at index EI_VERSION and for
+# Elf32_Ehdr.e_version
 EV_NONE = 0
 EV_CURRENT = 1
 
+# Constants for Elf32_Ehdr.e_ident at index EI_OSABI
 osabi_linux = 0x03
 
-EM_NONE = 0
-EM_386 = 3
-
-# Applicable values of Header32.e_type
+# Constants applicable for Elf32_Ehdr.e_type
 ET_NONE = 0
 ET_REL = 1
 ET_EXEC = 2
@@ -41,6 +51,42 @@ ET_DYN = 3
 ET_CORE = 4
 ET_LOPROC = 0xff00
 ET_HIPROC = 0xffff
+
+# Constants for e_machine (incomplete, I bet there are more)
+EM_NONE = 0
+EM_M32 = 1
+EM_SPARC = 2
+EM_386 = 3
+EM_68K = 4
+EM_88K = 5
+EM_unknown6 = 6
+EM_860 = 7
+EM_MIPS = 8
+
+
+class Elf32_Ehdr(ctypes.Structure):
+    """ELF header for 32 bit architectures."""
+    _packed_ = 1
+    _fields_ = [
+        ('e_ident', ctypes.c_uint8 * (4 + 1 + 1 + 1 + 1 + 1 + 7)),
+        ('e_type', ctypes.c_uint16),
+        ('e_machine', ctypes.c_uint16),
+        ('e_version', ctypes.c_uint32),
+        ('e_entry', ctypes.c_uint32),
+        ('e_phoff', ctypes.c_uint32),
+        ('e_shoff', ctypes.c_uint32),
+        ('e_flags', ctypes.c_uint32),
+        ('e_ehsize', ctypes.c_uint16),
+        ('e_phentsize', ctypes.c_uint16),
+        ('e_phnum', ctypes.c_uint16),
+        ('e_shentsize', ctypes.c_uint16),
+        ('e_shnum', ctypes.c_uint16),
+        ('e_shstrndx', ctypes.c_uint16),
+    ]
+
+
+assert ctypes.sizeof(Elf32_Ehdr) == 0x34
+
 
 PT_NULL = 0
 PT_LOAD = 1
@@ -52,12 +98,31 @@ PT_PHDR = 6
 PT_TLS = 7
 PT_NUM = 8
 PT_GNU_EH_FRAME = 0x6474e550
-PT_GNU_STACK    = 0x6474e551
-PT_GNU_RELRO    = 0x6474e552
+PT_GNU_STACK = 0x6474e551
+PT_GNU_RELRO = 0x6474e552
 
 PT_X = 1 << 0
 PT_W = 1 << 1
 PT_R = 1 << 2
+
+
+class Elf32_Phdr(ctypes.Structure):
+    """ELF program header for 32 bit architectures."""
+    _packed_ = 1
+    _fields_ = [
+        ('p_type', ctypes.c_uint32),
+        ('p_offset', ctypes.c_uint32),
+        ('p_vaddr', ctypes.c_uint32),
+        ('p_paddr', ctypes.c_uint32),
+        ('p_filesz', ctypes.c_uint32),
+        ('p_memsz', ctypes.c_uint32),
+        ('p_flags', ctypes.c_uint32),
+        ('p_align', ctypes.c_uint32),
+    ]
+
+
+assert ctypes.sizeof(Elf32_Phdr) == 0x20
+
 
 SHN_UNDEF = 0
 SHN_LORESERVE = 0xff00
@@ -90,72 +155,7 @@ SHF_EXECINSTR = 0x4
 SHF_MASKPROC = 0xf0000000
 
 
-class Header32(ctypes.Structure):
-    """ELF header for 32 bit architectures."""
-    _packed_ = 1
-    _fields_ = [
-        ('e_ident', ctypes.c_uint8 * (4 + 1 + 1 + 1 + 1 + 1 + 7)),
-        ('e_type', ctypes.c_uint16),
-        ('e_machine', ctypes.c_uint16),
-        ('e_version', ctypes.c_uint32),
-        ('e_entry', ctypes.c_uint32),
-        ('e_phoff', ctypes.c_uint32),
-        ('e_shoff', ctypes.c_uint32),
-        ('e_flags', ctypes.c_uint32),
-        ('e_ehsize', ctypes.c_uint16),
-        ('e_phentsize', ctypes.c_uint16),
-        ('e_phnum', ctypes.c_uint16),
-        ('e_shentsize', ctypes.c_uint16),
-        ('e_shnum', ctypes.c_uint16),
-        ('e_shstrndx', ctypes.c_uint16),
-    ]
-
-    @classmethod
-    def create_x86_linux_exec(cls):
-        """
-        Create a x86 (32bit) linux executable.
-
-        :returns:
-            A new header with some of the fields initialized.
-
-        Fields starting from ``e_entry`` are not initialized and have to be
-        filled by the caller.
-        """
-        self = cls()
-        self.e_ident[EI_MAG0] = 0x7F
-        self.e_ident[EI_MAG1] = ord('E')
-        self.e_ident[EI_MAG2] = ord('L')
-        self.e_ident[EI_MAG3] = ord('F')
-        self.e_ident[EI_CLASS] = ELFCLASS32
-        self.e_ident[EI_DATA] = ELFDATA2LSB
-        self.e_ident[EI_VERSION] = EV_CURRENT
-        self.e_ident[EI_OSABI] = osabi_linux
-        self.e_ident[EI_ABIVERSION] = 0
-        self.e_type = ET_EXEC
-        self.e_machine = EM_386
-        self.e_version = EV_CURRENT
-        return self
-
-
-class ProgramHeader32(ctypes.Structure):
-    """ELF program header for 32 bit architectures."""
-    _packed_ = 1
-    _fields_ = [
-        ('p_type', ctypes.c_uint32),
-        ('p_offset', ctypes.c_uint32),
-        ('p_vaddr', ctypes.c_uint32),
-        ('p_paddr', ctypes.c_uint32),
-        ('p_filesz', ctypes.c_uint32),
-        ('p_memsz', ctypes.c_uint32),
-        ('p_flags', ctypes.c_uint32),
-        ('p_align', ctypes.c_uint32),
-    ]
-
-
-assert ctypes.sizeof(ProgramHeader32) == 0x20
-
-
-class SectionHeader32(ctypes.Structure):
+class Elf32_Shdr(ctypes.Structure):
     """ELF section header for 32 bit architectures."""
     _packed_ = 1
     _fields_ = [
@@ -172,4 +172,97 @@ class SectionHeader32(ctypes.Structure):
     ]
 
 
-assert ctypes.sizeof(SectionHeader32) == 0x28
+assert ctypes.sizeof(Elf32_Shdr) == 0x28
+
+
+class Template(object):
+    """Template for setting various details of EFL data."""
+
+    def __init__(self, header):
+        self.header = header
+        if header.e_ident[EI_CLASS] == ELFCLASS32:
+            self.phdr_cls = Elf32_Phdr
+            self.shdr_cls = Elf32_Shdr
+        else:
+            raise NotImplementedError
+
+
+X86StaticExec = Template(
+    header=Elf32_Ehdr(
+        e_ident=(ctypes.c_uint8 * 16)(
+            0x7F,
+            ord('E'),
+            ord('L'),
+            ord('F'),
+            ELFCLASS32,
+            ELFDATA2LSB,
+            EV_CURRENT,
+            osabi_linux,
+        ),
+        e_type=ET_EXEC,
+        e_machine=EM_386,
+        e_version=EV_CURRENT,
+    ))
+
+
+class Builder(object):
+    """Builder for ELF files."""
+
+    def __init__(self, stream, template):
+        if isinstance(stream, file):
+            raise TypeError("stream is not a file")
+        if stream.mode != 'wb':
+            raise ValueError("stream must be open in writable binary mode")
+        if not isinstance(template, Template):
+            raise TypeError("template is not an ElfTemplate")
+        self._stream = stream
+        self._template = template
+        self._program_headers = []
+        self._section_headers = []
+        self._data_fns = []
+
+    def build(self):
+        header = copy.deepcopy(self._template.header)
+        # NOTE: header.e_ident is setup by the template
+        # NOTE: header.e_type is setup by the template
+        # NOTE: header.e_machine is setup by the template
+        # NOTE: header.e_version is setup by the template
+        # TODO: header.e_entry = ...
+        header.e_phoff = ctypes.sizeof(header)  # The program header is
+                                                # traditionally just after the
+                                                # elf header.
+        # TODO: header.e_shoff = ...
+        # TODO: header.e_flags = ...
+        header.e_ehsize = ctypes.sizeof(header)
+        header.e_phentsize = ctypes.sizeof(self._template.phdr_cls)
+        header.e_phnum = len(self._program_headers)
+        header.e_shentsize = ctypes.sizeof(self._template.shdr_cls)
+        header.e_shnum = len(self._section_headers)
+        # TODO: header.e_shstrndx = ...
+        off = self._stream.write(header)
+        for phdr in self._program_headers:
+            # phdr.p_offset = ...
+            off += self._stream.write(phdr)
+        for blob_fn in self._blob_fns:
+            blob = blob_fn()
+            off += self._stream.write(blob)
+        for shdr in self._section_headers:
+            # TODO: tie section header with written data
+            off += self._stream.write(shdr)
+        return off
+
+    def add_program_header(self):
+        phdr = self.template.phdr_cls()
+        self._program_headers.append(phdr)
+        return phdr
+
+    def add_section_header(self):
+        # Inject reserved zero-filled section header
+        if len(self._section_headers) == 0:
+            self._section_headers.append(self.template.shdr_cls())
+        shdr = self.template.shdr_cls()
+        self._section_headers.append(shdr)
+        return shdr
+
+    def add_data(self, blob_fn):
+        self._data_fns.append(blob_fn)
--- schnibble/test_elf.py
+++ schnibble/test_elf.py
@@ -5,11 +5,11 @@ import unittest
 
 from . import elf
 
-class Header32Tests(unittest.TestCase):
+class Elf32_EhdrTests(unittest.TestCase):
 
     def test_e_ident(self):
-        self.assertEqual(elf.Header32.e_ident.offset, 0)
-        self.assertEqual(elf.Header32.e_ident.size, 16)
+        self.assertEqual(elf.Elf32_Ehdr.e_ident.offset, 0)
+        self.assertEqual(elf.Elf32_Ehdr.e_ident.size, 16)
 
     def test_EI_MAG0_4(self):
         self.assertEqual(elf.EI_MAG0, 0)
@@ -36,66 +36,56 @@ class Header32Tests(unittest.TestCase):
         self.assertEqual(elf.EI_PAD, 0x09)
 
     def test_e_type(self):
-        self.assertEqual(elf.Header32.e_type.offset, 0x10)
-        self.assertEqual(elf.Header32.e_type.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_type.offset, 0x10)
+        self.assertEqual(elf.Elf32_Ehdr.e_type.size, 2)
 
     def test_e_machine(self):
-        self.assertEqual(elf.Header32.e_machine.offset, 0x12)
-        self.assertEqual(elf.Header32.e_machine.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_machine.offset, 0x12)
+        self.assertEqual(elf.Elf32_Ehdr.e_machine.size, 2)
 
     def test_e_version(self):
-        self.assertEqual(elf.Header32.e_version.offset, 0x14)
-        self.assertEqual(elf.Header32.e_version.size, 4)
+        self.assertEqual(elf.Elf32_Ehdr.e_version.offset, 0x14)
+        self.assertEqual(elf.Elf32_Ehdr.e_version.size, 4)
 
     def test_e_entry(self):
-        self.assertEqual(elf.Header32.e_entry.offset, 0x18)
-        self.assertEqual(elf.Header32.e_entry.size, 4)
+        self.assertEqual(elf.Elf32_Ehdr.e_entry.offset, 0x18)
+        self.assertEqual(elf.Elf32_Ehdr.e_entry.size, 4)
 
     def test_e_phoff(self):
-        self.assertEqual(elf.Header32.e_phoff.offset, 0x1C)
-        self.assertEqual(elf.Header32.e_phoff.size, 4)
+        self.assertEqual(elf.Elf32_Ehdr.e_phoff.offset, 0x1C)
+        self.assertEqual(elf.Elf32_Ehdr.e_phoff.size, 4)
 
     def test_e_shoff(self):
-        self.assertEqual(elf.Header32.e_shoff.offset, 0x20)
-        self.assertEqual(elf.Header32.e_shoff.size, 4)
+        self.assertEqual(elf.Elf32_Ehdr.e_shoff.offset, 0x20)
+        self.assertEqual(elf.Elf32_Ehdr.e_shoff.size, 4)
 
     def test_e_flags(self):
-        self.assertEqual(elf.Header32.e_flags.offset, 0x24)
-        self.assertEqual(elf.Header32.e_flags.size, 4)
+        self.assertEqual(elf.Elf32_Ehdr.e_flags.offset, 0x24)
+        self.assertEqual(elf.Elf32_Ehdr.e_flags.size, 4)
 
     def test_e_ehsize(self):
-        self.assertEqual(elf.Header32.e_ehsize.offset, 0x28)
-        self.assertEqual(elf.Header32.e_ehsize.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_ehsize.offset, 0x28)
+        self.assertEqual(elf.Elf32_Ehdr.e_ehsize.size, 2)
 
     def test_e_phentsize(self):
-        self.assertEqual(elf.Header32.e_phentsize.offset, 0x2a)
-        self.assertEqual(elf.Header32.e_phentsize.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_phentsize.offset, 0x2a)
+        self.assertEqual(elf.Elf32_Ehdr.e_phentsize.size, 2)
 
     def test_e_phnum(self):
-        self.assertEqual(elf.Header32.e_phnum.offset, 0x2c)
-        self.assertEqual(elf.Header32.e_phnum.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_phnum.offset, 0x2c)
+        self.assertEqual(elf.Elf32_Ehdr.e_phnum.size, 2)
 
     def test_e_shentsize(self):
-        self.assertEqual(elf.Header32.e_shentsize.offset, 0x2e)
-        self.assertEqual(elf.Header32.e_shentsize.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_shentsize.offset, 0x2e)
+        self.assertEqual(elf.Elf32_Ehdr.e_shentsize.size, 2)
 
     def test_e_shnum(self):
-        self.assertEqual(elf.Header32.e_shnum.offset, 0x30)
-        self.assertEqual(elf.Header32.e_shnum.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_shnum.offset, 0x30)
+        self.assertEqual(elf.Elf32_Ehdr.e_shnum.size, 2)
 
     def test_e_shstrndx(self):
-        self.assertEqual(elf.Header32.e_shstrndx.offset, 0x32)
-        self.assertEqual(elf.Header32.e_shstrndx.size, 2)
+        self.assertEqual(elf.Elf32_Ehdr.e_shstrndx.offset, 0x32)
+        self.assertEqual(elf.Elf32_Ehdr.e_shstrndx.size, 2)
 
     def test_total_size(self):
-        self.assertEqual(ctypes.sizeof(elf.Header32), 52)
-
-    def test_create_x86_linux_exec(self):
-        header = elf.Header32.create_x86_linux_exec()
-        self.assertEqual(
-            list(header.e_ident),
-            [0x7f, ord('E'), ord('L'), ord('F'), 1,  1, 1, 3, 0,
-             0, 0, 0, 0, 0, 0, 0])
-        self.assertEqual(header.e_type, 2)
-        self.assertEqual(header.e_machine, 0x03)
-        self.assertEqual(header.e_version, 1)
+        self.assertEqual(ctypes.sizeof(elf.Elf32_Ehdr), 52)
