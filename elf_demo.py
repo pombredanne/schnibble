7d6fef42fb36d9a87e1203c0237e05eef8247865
Zygmunt Krynicki <me@zygoon.pl>
2016-04-02 23:26:33 +0200

    Rough idea on how elf demo might work
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- elf_demo.py
+++ elf_demo.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+"""Work-in-progress on elf generator."""
+
 
 def main():
+    # get an elf Builder
+    # inject some data into the file
+    # map the data to section and program headers
     pass
 
 if __name__ == "__main__":
