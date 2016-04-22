f476051dd9c1cc550012a2a81a38bfb550d70f9e
Zygmunt Krynicki <me@zygoon.pl>
2016-04-22 23:57:06 +0200

    Fix flake8 issues

--- schnibble/arm/arm.py
+++ schnibble/arm/arm.py
@@ -1,13 +1,14 @@
-"""
-Work-in-progress on ARM assembly
-"""
+"""Work-in-progress on ARM assembly."""
+
 
 class Flags(object):
+
     """ARM status flags (userspace part)."""
-    Negative = 1<<31
-    Zero = 1<<30
-    Carry = 1<<29
-    Overflow = 1<<28
+
+    Negative = 1 << 31
+    Zero = 1 << 30
+    Carry = 1 << 29
+    Overflow = 1 << 28
 
     N = Negative
     Z = Zero
