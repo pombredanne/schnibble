d37cc4299b7f0967628c5a9dc419efc7c90eb9f1
Zygmunt Krynicki <me@zygoon.pl>
2016-04-22 23:47:11 +0200

    Add arm flags register
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- schnibble/arm/arm.py
+++ schnibble/arm/arm.py
@@ -1,3 +1,15 @@
 """
 Work-in-progress on ARM assembly
 """
+
+class Flags(object):
+    """ARM status flags (userspace part)."""
+    Negative = 1<<31
+    Zero = 1<<30
+    Carry = 1<<29
+    Overflow = 1<<28
+
+    N = Negative
+    Z = Zero
+    C = Carry
+    V = Overflow
