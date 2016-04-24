10afe6fc6322d4b771ad4630554685692b1ed6bb
Zygmunt Krynicki <me@zygoon.pl>
2016-04-24 23:59:15 +0200

    Add arm encoding
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- schnibble/arm/arm.py
+++ schnibble/arm/arm.py
@@ -1,5 +1,7 @@
 """Work-in-progress on ARM assembly."""
 
+import ctypes
+
 
 class Flags(object):
     """ARM status flags (userspace part)."""
@@ -13,3 +15,14 @@ class Flags(object):
     Z = Zero
     C = Carry
     V = Overflow
+
+
+class arm_enc(ctypes.Structure):
+    _fields_ = [
+        ('_f1', ctypes.c_uint32, 4),
+        ('op', ctypes.c_uint32, 19),
+        ('_f2', ctypes.c_uint32, 4),
+        ('op1', ctypes.c_uint32, 3),
+        ('cond', ctypes.c_uint32, 4),
+    ]
+    _packed_ = 1
