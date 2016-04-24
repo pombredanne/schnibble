cf42d95fe2b339d97bc3dcbc11c9bc0657731133
Zygmunt Krynicki <me@zygoon.pl>
2016-04-25 00:12:56 +0200

    Add constants for arm condition codes
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- schnibble/arm/arm.py
+++ schnibble/arm/arm.py
@@ -30,3 +30,57 @@ class arm_enc(ctypes.Structure):
     _packed_ = 1
 
 assert ctypes.sizeof(arm_enc) == 4
+
+
+class cond:
+    """
+    Conditional codes.
+
+    Constants defined in this class correspond to possible values of the
+    four-bit condition field available in instructions using the ARM ISA.
+    """
+
+    #: Equal
+    EQ = 0b0000
+
+    #: Not equal
+    NE = 0b0001
+
+    #: Carry set
+    CS = 0b0010
+
+    #: Carry clear
+    CC = 0b0011
+
+    #: Minus, negative
+    MI = 0b0100
+
+    #: Plus, positive or zero
+    PL = 0b0101
+
+    #: Overflow
+    VS = 0b0110
+
+    #: No overflow
+    VC = 0b0111
+
+    #: Unsigned higher
+    HI = 0b1000
+
+    #: Unsigned lower or same
+    LS = 0b1001
+
+    #: Signed greater than or equal
+    GE = 0b1010
+
+    #: Signed less than
+    LT = 0b1011
+
+    #: Signed greater than
+    GT = 0b1100
+
+    #: Signed less than or equal
+    LE = 0b1101
+
+    #: Always (unconditional)
+    AL = 0b1110
