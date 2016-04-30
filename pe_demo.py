e05074b2c23f547cb3db6f81ffeefafd085b965e
Zygmunt Krynicki <me@zygoon.pl>
2016-04-30 23:51:02 +0200

    Move arch code to dedicated package
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- pe_demo.py
+++ pe_demo.py
@@ -11,9 +11,9 @@ import sys
 from ctypes import sizeof
 
 from schnibble.binfmt import pe
-from schnibble.x86.instructions import ADD, MOV, RET
-from schnibble.x86.operands import imm32
-from schnibble.x86.registers import EAX
+from schnibble.arch.x86.instructions import ADD, MOV, RET
+from schnibble.arch.x86.operands import imm32
+from schnibble.arch.x86.registers import EAX
 
 
 def emit_code(*asm):
